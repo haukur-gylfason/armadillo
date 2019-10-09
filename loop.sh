3c035bcb572e0c56c753e0f386431d2f4f933dda
Haukur Gylfason <haukur.gylfason@arm.com>
2019-10-09 12:03:18 +0200

    Script 11

--- loop.sh
+++ loop.sh
@@ -1,11 +1,13 @@
 #! /bin/bash 
 
 
-
-for i in {1..10}
+for i in {11..20}
 do
 	echo "Haukur Gylfason" >> haukur.txt
 	git add haukur.txt
 	git commit -m "Script ${i}"
 done
 
+git fetch 
+git rebase
+git push
